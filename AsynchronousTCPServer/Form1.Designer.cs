c081af7b57af4c9293eb2471e007fe8a3553e4a7
matiwa <mateuszivan1994@wp.pl>
2022-03-24 17:45:38 +0100

    first commit

---
+++ AsynchronousTCPServer/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace AsynchronousTCPServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BStop = new System.Windows.Forms.Button();
+            this.BStart = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BStop
+            // 
+            this.BStop.Location = new System.Drawing.Point(96, 289);
+            this.BStop.Name = "BStop";
+            this.BStop.Size = new System.Drawing.Size(75, 23);
+            this.BStop.TabIndex = 13;
+            this.BStop.Text = "Stop";
+            this.BStop.UseVisualStyleBackColor = true;
+            this.BStop.Click += new System.EventHandler(this.BStop_Click);
+            // 
+            // BStart
+            // 
+            this.BStart.Location = new System.Drawing.Point(15, 289);
+            this.BStart.Name = "BStart";
+            this.BStart.Size = new System.Drawing.Size(75, 23);
+            this.BStart.TabIndex = 12;
+            this.BStart.Text = "Start";
+            this.BStart.UseVisualStyleBackColor = true;
+            this.BStart.Click += new System.EventHandler(this.BStart_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(15, 32);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(372, 251);
+            this.listBox1.TabIndex = 11;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(267, 6);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(232, 8);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Port:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(79, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(145, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "IP Address:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(401, 321);
+            this.Controls.Add(this.BStop);
+            this.Controls.Add(this.BStart);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "AsynchronousTCPServer";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BStop;
+        private System.Windows.Forms.Button BStart;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ AsynchronousTCPServer/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AsynchronousTCPServer
+{
+    public partial class Form1 : Form
+    {
+        private TcpListener serwer;
+        private TcpClient klient;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BStart_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("Waiting for connection ...");
+            IPAddress adresIP;
+            try
+            {
+                adresIP = IPAddress.Parse(textBox1.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Wrong IP address format!", "Error");
+                textBox1.Text = String.Empty;
+                return;
+            }
+            int port = System.Convert.ToInt16(numericUpDown1.Value);
+            try
+            {
+                serwer = new TcpListener(adresIP, port);
+                serwer.Start();
+                serwer.BeginAcceptTcpClient(new AsyncCallback(AcceptTcpClientCallback),
+                serwer);
+            }
+            catch (Exception ex)
+            {
+                listBox1.Items.Add("Error: " + ex.Message);
+            }
+        }
+
+        private void BStop_Click(object sender, EventArgs e)
+        {
+            if(serwer != null) serwer.Stop();
+        }
+
+        private void AcceptTcpClientCallback(IAsyncResult asyncResult)
+        {
+            TcpListener s = (TcpListener)asyncResult.AsyncState;
+            klient = s.EndAcceptTcpClient(asyncResult);
+            SetListBoxText("Connection was successful!");
+            klient.Close();
+            serwer.Stop();
+        }
+
+        private delegate void SetTextCallBack(string tekst);
+        private void SetListBoxText(string tekst)
+        {
+            if (listBox1.InvokeRequired)
+            {
+                SetTextCallBack f = new SetTextCallBack(SetListBoxText);
+                this.Invoke(f, new object[] { tekst });
+            }
+            else
+            {
+                listBox1.Items.Add(tekst);
+            }
+        }
+    }
+}
